d06471dd5b8dfe2aa96e270738eea5f960564e22
Thiago Soares <thiagocarv4@gmail.com>
2025-04-18 20:04:46 -0300

    Middleware de Autenticação

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
 class ProductsController {
   async index(request: Request, response: Response) {
-    return response.json({message: "products"})
+    return response.json({ message: "products" });
   }
 
   async create(request: Request, response: Response) {
-    return response.json({message: "created"})
+    return response.json({ message: request.user?.id });
   }
 }
 
-export { ProductsController }
+export { ProductsController };
