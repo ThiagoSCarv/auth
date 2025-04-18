f416d59c959e92d4389d74f024226486453c5b1a
Thiago Soares <thiagocarv4@gmail.com>
2025-04-18 19:32:07 -0300

    Criando sessão

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,30 @@
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    response.status(201).json({ message: "ok" });
+    const { username, password } = request.body;
+
+    const fakeUser = {
+      id: 1,
+      username: "thiago",
+      password: "123456",
+    };
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("usuario e/ou senha incorreta!", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id)
+    });
+
+    return response.json({ token });
   }
 }
 
